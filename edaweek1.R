a5ff3304ba36da6439d0483c86a26872b9515c0f
teddyhla <teddyhla@gmail.com>
2021-10-26 15:18:37 +0100

    initial commit

---
+++ edaweek1.R
@@ -0,0 +1,7 @@
+# task1 valid github url containing repository
+# task2 need to contain commit to the repo
+# task3 are there plot files
+# task4 are the plot files correct
+# task5 correct Rcodes for each plot files
+# note for each plot need to save it to a PNG file, with width and height - 480pixels
+#name in plot 1.pmg 
